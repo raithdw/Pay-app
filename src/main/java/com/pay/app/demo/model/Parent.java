fecad83218625d0505b61ab99ff2ea529c5e6da1
catalin.fosalau <fosalaucatalin@gmail.com>
2025-01-31 12:17:54 +0200

    TODOs

--- src/main/java/com/pay/app/demo/model/Parent.java
+++ src/main/java/com/pay/app/demo/model/Parent.java
@@ -1,7 +1,11 @@
 package com.pay.app.demo.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +13,7 @@ import java.util.List;
 public class Parent {
 
     @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String email;
@@ -17,10 +22,30 @@ public class Parent {
     private List<Long> accountIds = new ArrayList<>();
     private List<Long> cardIds = new ArrayList<>();
 
+
+    /**
+     * FOSI: I don't think that we need separate Parent / Child tables.
+     * Let's store a list of parents (a child can have 2 parents) and a list of children in this entity (maybe rename it to "Client")
+     * -----------------------------------------------------------------------------------------------------------------
+     * FOSI: The magic of Hibernate / JPA is that we don't store list of ids.
+     * We store list of the entities that we are mapping to, and hibernate will auto-magically do the binding using the id columns for us
+     * -----------------------------------------------------------------------------------------------------------------
+     * FOSI: Let's double check the entity mappings
+     * A client has 1/M accounts
+     * An account is owned by a single client
+     * A card points to a single account
+     * A card is owned by a single client
+     * A client can have 1/M card
+     * For an account we can have multiple cards
+     * -----------------------------------------------------------------------------------------------------------------
+     * FOSI: You familiar with Lombok?
+     * -----------------------------------------------------------------------------------------------------------------
+     * FOSI: ID autogeneration
+     */
+
     // Constructors
     public Parent() {}
-    public Parent(Long id, String name, String email) {
-        this.id = id;
+    public Parent(String name, String email) {
         this.name = name;
         this.email = email;
     }
